646c86da3a294fd9b9050a0c362edf8975897b1c
pujakesuma <ahmadridwan.3107@gmail.com>
2020-01-28 12:53:46 +0700

    updated

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,14 +4,12 @@ const Route = express.Router();
 const companies = require('./companies');
 const engineers = require('./engineer');
 const search = require('./search');
-const skill = require('./skill');
 const auth = require('./userAuth');
 
 //Routes for this API
 Route.use('/api/companies', companies);
 Route.use('/api/engineers', engineers);
 Route.use('/api/search', search);
-Route.use('/api/skill', skill);
 Route.use('/userAuth', auth);
 
 module.exports = Route;
